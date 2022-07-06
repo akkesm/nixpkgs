912b06ef005f4dfb727db194c10f8b4c66445001
Alessandro Barenghi <alessandro.barenghi@tuta.io>
2022-07-06 11:31:50 +0200

    shntool: use Debian sources and patches

--- pkgs/applications/audio/shntool/default.nix
+++ pkgs/applications/audio/shntool/default.nix
@@ -1,15 +1,29 @@
-{ lib, stdenv, fetchurl, flac }:
+{ lib
+, stdenv
+, fetchFromGitLab
+, flac
+}:
 
-stdenv.mkDerivation {
-  version = "3.0.10";
+stdenv.mkDerivation rec {
   pname = "shntool";
+  version = "3.0.10";
 
-  src = fetchurl {
-    url = "http://www.etree.org/shnutils/shntool/dist/src/shntool-3.0.10.tar.gz";
-    sha256 = "00i1rbjaaws3drkhiczaign3lnbhr161b7rbnjr8z83w8yn2wc3l";
+  src = fetchFromGitLab {
+    domain = "salsa.debian.org";
+    owner = "debian";
+    repo = "shntool";
+    rev = "18a31ab331aadb706978fd2e67418c0e740385ac";
+    sha256 = "1g28qvl0vvxy8sdlvq665iz1s6j3pmkxlfkrdj4sfzsb8khixyj5";
   };
 
   buildInputs = [ flac ];
+
+  patches = [
+    "${src}/debian/patches/950803.patch"
+    "${src}/debian/patches/large-size.patch"
+    "${src}/debian/patches/large-times.patch"
+    "${src}/debian/patches/no-cdquality-check.patch"
+  ];
 
   meta = {
     description = "Multi-purpose WAVE data processing and reporting utility";
